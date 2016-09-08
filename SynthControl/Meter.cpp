00f6e1e1d7828427091b51f7c161280450749f91
Colin Sowter <csowter@gmail.com>
2016-09-08 22:18:33 +0100

    Meter painting using colours

--- SynthControl/Meter.cpp
+++ SynthControl/Meter.cpp
@@ -17,13 +17,42 @@ void cMeter::CreateTextures(SDL_Renderer *renderer)
 	mMeterOffTexture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_TARGET, GetBounds().w, GetBounds().h);
 	mMeterOnTexture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_TARGET, GetBounds().w, GetBounds().h);
 
+	SDL_Rect rect;
+	rect.w = GetBounds().w;
+	rect.x = 0;
+
+	//red
 	SDL_SetRenderTarget(renderer, mMeterOffTexture);
+	SDL_SetRenderDrawColor(renderer, 55, 0, 0, 255);
+	SDL_RenderClear(renderer);
+
+	//green
+	SDL_SetRenderDrawColor(renderer, 0, 55, 0, 255);
+	rect.y = GetBounds().h - 0.7f * GetBounds().h;
+	rect.h = 0.7f * GetBounds().h;
+	SDL_RenderFillRect(renderer, &rect);
+
+	//amber
+	SDL_SetRenderDrawColor(renderer, 55, 55, 0, 255);
+	rect.y = GetBounds().h - 0.9f * GetBounds().h;
+	rect.h = 0.2f * GetBounds().h;
+	SDL_RenderFillRect(renderer, &rect);
+
+	//red
+	SDL_SetRenderTarget(renderer, mMeterOnTexture);
 	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 	SDL_RenderClear(renderer);
 
-	SDL_SetRenderTarget(renderer, mMeterOnTexture);
-	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-	SDL_RenderClear(renderer);
+	SDL_SetRenderDrawColor(renderer, 0, 185, 0, 255);
+	rect.y = GetBounds().h - 0.7f * GetBounds().h;
+	rect.h = 0.7f * GetBounds().h;
+	SDL_RenderFillRect(renderer, &rect);
+
+	//amber
+	SDL_SetRenderDrawColor(renderer, 155, 155, 0, 255);
+	rect.y = GetBounds().h - 0.9f * GetBounds().h;
+	rect.h = 0.2f * GetBounds().h;
+	SDL_RenderFillRect(renderer, &rect);
 
 	SDL_SetRenderTarget(renderer, NULL);
 }
@@ -39,7 +68,7 @@ void cMeter::Render(SDL_Renderer *renderer)
 
 	SDL_RenderCopy(renderer, mMeterOffTexture, NULL, &mBoundingRectangle);
 
-	SDL_Rect srcOnRectangle = { 0, 0, mBoundingRectangle.w, mMeterValue * mBoundingRectangle.h };
-	SDL_Rect dstOnRectangle = { mBoundingRectangle.x, mBoundingRectangle.y + mBoundingRectangle.h - (mMeterValue * mBoundingRectangle.h), mBoundingRectangle.w, mMeterValue * mBoundingRectangle.h };
+	SDL_Rect srcOnRectangle = { 0, mBoundingRectangle.h - (mMeterValue * mBoundingRectangle.h), mBoundingRectangle.w, mMeterValue * mBoundingRectangle.h };
+	SDL_Rect dstOnRectangle = { mBoundingRectangle.x, mBoundingRectangle.y + mBoundingRectangle.h - (mMeterValue * mBoundingRectangle.h) + 1, mBoundingRectangle.w, mMeterValue * mBoundingRectangle.h };
 	SDL_RenderCopy(renderer, mMeterOnTexture, &srcOnRectangle, &dstOnRectangle);
 }
